6a5ffc4993df132c1c7554a75315293fab44a2f2
Zeeshan Junaid <zeeshanjunaid2222@gmail.com>
2020-09-25 01:52:01 +0500

    small changes for canvas

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -4,8 +4,8 @@ export default function useWindowSize() {
   function getSize() {
     if (typeof window === "undefined") {
       return {
-        width: "100vw",
-        height: "100vh",
+        width: "",
+        height: "",
       }
     } else {
       return {
--- src/styles/homeStyles.js
+++ src/styles/homeStyles.js
@@ -25,7 +25,6 @@ export const Canvas = styled.canvas`
   top: 0;
   left: 0;
   height: 100%;
-  width: 100%;
   display: block;
 `
 
